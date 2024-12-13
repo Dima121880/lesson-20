3391a6cf844d41bf2169b2db46ae308b7445265f
Dima121880 <surguladze.dima33@gmail.com>
2024-12-14 00:21:06 +0400

    add clock

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,40 @@
 // davaleba-1
 
-function startTime() {
-  const today = new Date();
-  let h = today.getHours();
-  let m = today.getMinutes();
-  let s = today.getSeconds();
-  m = checkTime(m);
-  s = checkTime(s);
-  document.getElementById("txt").innerHTML = h + ":" + m + ":" + s;
-  setTimeout(startTime, 1000);
-}
+// function startTime() {
+//   const today = new Date();
+//   let h = today.getHours();
+//   let m = today.getMinutes();
+//   let s = today.getSeconds();
+//   m = checkTime(m);
+//   s = checkTime(s);
+//   document.getElementById("txt").innerHTML = h + ":" + m + ":" + s;
+//   setTimeout(startTime, 1000);
+// }
 
-function checkTime(i) {
-  if (i < 10) {
-    i = "0" + i;
+// function checkTime(i) {
+//   if (i < 10) {
+//     i = "0" + i;
+//   }
+//   return i;
+// }
+
+let countDownDate = new Date("Jan 5, 2030 15:37:25").getTime();
+
+let x = setInterval(function () {
+  let now = new Date().getTime();
+
+  let distance = countDownDate - now;
+
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("demo").innerHTML =
+    days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
   }
-  return i;
-}
+}, 1000);
